f48450a4553bd00fa7490a001a605eecb445f470
AdrianPC <adrianchooo18@gmail.com>
2021-10-11 09:58:33 -0500

    Login mobile , logo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,41 @@
 import React from "react";
+import Logo from "../../assets/images/MercaTodoLogo.png";
 import "./Login.scss";
 
 const Login = () => {
+    //event prevet page reload
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+    };
     return (
         <>
             <div className="container">
+                <div className="header-logo">
+                    <img src={Logo} alt="LogoMercaTodo" className="Logo" />
+                    <p className="Calidad">Calidad y confianza</p>
+                </div>
                 <div className="app-wrapper">
                     <form method="post" className="form-wrapper">
                         <div className="user">
                             <label className="label">Usuario</label>
-                            <input className="input" type="text" required />
+                            <input
+                                className="input"
+                                type="text"
+                                name="username"
+                            />
                         </div>
                         <div className="password">
                             <label className="label">Contraseña</label>
-                            <input className="input" type="password" required />
+                            <input
+                                className="input"
+                                type="password"
+                                name="password"
+                            />
                         </div>
                         <div className="forgot">Olvidaste tú contraseña?</div>
-                        <input type="submit" value="Login" />
+                        <button className="submit" onClick={handleFormSubmit}>
+                            LOGIN
+                        </button>
                     </form>
                 </div>
             </div>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Login from '../components/Login/Login';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "../components/Login/Login";
 
 // // const App = () => <h1>Hello React!!!</h1>
 const App = () => {
     return (
-        <div>
-            <Login />
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route extact path="/" component={Login} />
+            </Switch>
+        </BrowserRouter>
     );
 };
 
