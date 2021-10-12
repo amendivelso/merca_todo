e23e465dea782403e3a78c5194e53190318fcb8e
AdrianPC <adrianchooo18@gmail.com>
2021-10-11 22:02:56 -0500

    View user basic

--- src/Pages/Home/User/User.jsx
+++ src/Pages/Home/User/User.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "../../../components/Header/Header";
+import Search from "../../../components/Search/Search";
+import TableData from "../../../components/Table/Table";
 
 const User = () => {
     return (
