217595eb0a445ba49529ab2e1254af8d87f70ce2
AdrianPC <adrianchooo18@gmail.com>
2021-10-13 11:49:30 -0500

    auth upload , fixed button

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Logo from "../../assets/images/LogoMobile.jpg";
 import "./Login.scss";
-import {ApiURL} from '../../services/apirest';
+import axios from 'axios';
+import md5 from 'md5';
+import  Cookies from 'universal-cookie';
+
+const baseUrl = "https://warm-garden-17574.herokuapp.com/api/employees";
+const cookies = new Cookies();
 
 class Login extends React.Component {
     state = {
@@ -9,6 +14,8 @@ class Login extends React.Component {
             username: "",
             password: "",
         },
+        error: false,
+        errorMsg: ""
     };
 
     //Event catch change on input
@@ -21,15 +28,49 @@ class Login extends React.Component {
         });
         // console.log(this.state.form);
     };
-    logSeccion = () => {
-        let url = ApiURL + "auth";
-        axios.post(url,this.state.form)
-        .then(response => {
-            console.log(response)
-        })
 
-    }
+    // const [data, setData] = useState([]);
+    //     const requestGet = async () => {
+    //         try {
+    //             await axios.get(ApiURL)
+    //                 .then(response => {
+    //                     setData(response.data);
+    //                 })
+    //         }
+    //         catch (err) {
+    //             console.log(err);
+    //         }
 
+    //     }
+    //     useEffect(async () => {
+    //         await requestGet();
+    //     }, [])
+
+    // logSeccion = () => {
+    //     let url = ApiURL + "auth";
+    //     axios.get(url, this.state.form).then((response) => {
+    //         console.log(response);
+    //     });
+    // };
+     logSeccion = () => {
+         axios.get(baseUrl, {params: {username: this.state.username, password: md5(this.state.form.password)}})
+         .then(response => {
+             return response.data;
+         })
+         .then(response => {
+             if (response.length >0 ) {
+                 let answer = response[0];
+                 cookies.set('username', answer.username, {path: "/"});
+                 alert(`Bienvenido ${answer.username}`);
+
+             } else {
+                 alert('Usuario o contraseña incorrecta');
+             }
+         })
+         .catch(error => {
+             console.log(error);
+         })
+     }
     render() {
         return (
             <>
@@ -41,14 +82,15 @@ class Login extends React.Component {
                         <p className="calidad">Calidad y confianza</p>
                     </div>
                     <div className="app-wrapper">
-                        <form method="post" className="form-wrapper">
+                        <form className="form-wrapper">
                             <div className="user">
                                 <label className="label">Usuario</label>
                                 <input
                                     className="input"
                                     type="text"
                                     name="username"
-                                    onChange={this.handleChange}/>
+                                    onChange={this.handleChange}
+                                />
                             </div>
                             <div className="password">
                                 <label className="label">Contraseña</label>
@@ -56,14 +98,16 @@ class Login extends React.Component {
                                     className="input"
                                     type="password"
                                     name="password"
-                                    onChange={this.handleChange}/>
+                                    onChange={this.handleChange}
+                                />
                             </div>
                             <div className="forgot">
                                 Olvidaste tú contraseña?
                             </div>
                             <button
                                 className="btn-sub"
-                                onClick={this.logSeccion}>
+                                onClick={() => this.logSeccion()}
+                            >
                                 LOGIN
                             </button>
                         </form>
--- src/services/apirest.js
+++ src/services/apirest.js
@@ -1,1 +1,1 @@
-export const ApiURL = "https://warm-garden-17574.herokuapp.com/api/employees/admin"
+export const ApiURL = "https://warm-garden-17574.herokuapp.com/api/employees"
