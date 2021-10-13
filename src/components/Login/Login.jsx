a086807b0c76337cd154bd5f6c93a74845553dc4
AdrianPC <adrianchooo18@gmail.com>
2021-10-12 20:51:13 -0500

    validation inputs

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,75 +1,95 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Logo from "../../assets/images/LogoMobile.jpg";
 import "./Login.scss";
 import axios from 'axios';
-import md5 from 'md5';
-import Cookies from 'universal-cookie';
-
 
 //Init const URL and COOKIES
 const baseUrl="https://warm-garden-17574.herokuapp.com/api/employees/admin";
-const cookies = new Cookies();
+// const cookies = new Cookies();
 
 class Login extends Component {
     state = {
         form: {
-            username: '',
-            password: ''
-        }
+            username: "",
+            password: "",
+        },
     };
 
     //Event catch change on input
-    handleChange = async e => {
-        await this.setState({
-            form:{
-                ...this.state.form, [e.target.name]: e.target.value
+    handleChange = (event) => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [event.target.name]: event.target.value
             }
         });
+        console.log(this.state.form);
     };
-    
+
     //Init Session
-    iniciaSesion = async() => {
+    iniciarSesion=async()=>{
+        await axios.get(baseUrl, {params: {username: this.state.form.username, password: md5(this.state.form.password)}})
+        .then(response=>{
+            return response.data;
+        })
+        .then(response=>{
+            if(response.length>0){
+                var respuesta=response[0];
+                cookies.set('id', respuesta.id, {path: "/"});
+                cookies.set('nombre', respuesta.nombre, {path: "/"});
+                cookies.set('username', respuesta.username, {path: "/"});
+                alert(`Bienvenido ${respuesta.nombre} ${respuesta.apellido_paterno}`);
+                window.location.href="./Admin";
+            }else{
+                alert('El usuario o la contraseña no son correctos');
+            }
+        })
+        .catch(error=>{
+            console.log(error);
+        })
 
     }
 
-    render(){
-    return (
-        <>
-            <div className="container">
-                <div className="header-logo">
-                    <img src={Logo} alt="LogoMercaTodo" className="Logo" />
+    render() {
+        return (
+            <>
+                <div className="container">
+                    <div className="header-logo">
+                        <img src={Logo} alt="LogoMercaTodo" className="Logo" />
+                    </div>
+                    <div className="calidad-div">
+                        <p className="calidad">Calidad y confianza</p>
+                    </div>
+                    <div className="app-wrapper">
+                        <form method="post" className="form-wrapper">
+                            <div className="user">
+                                <label className="label">Usuario</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="username"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="password">
+                                <label className="label">Contraseña</label>
+                                <input
+                                    className="input"
+                                    type="password"
+                                    name="password"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="forgot">
+                                Olvidaste tú contraseña?
+                            </div>
+                            <button className="submit">LOGIN</button>
+                        </form>
+                    </div>
                 </div>
-                <div className="calidad-div">
-                    <p className="calidad">Calidad y confianza</p>
-                </div>
-                <div className="app-wrapper">
-                    <form method="post" className="form-wrapper">
-                        <div className="user">
-                            <label className="label">Usuario</label>
-                            <input
-                                className="input"
-                                type="text" 
-                                name="username" 
-                            />
-                        </div>
-                        <div className="password">
-                            <label className="label">Contraseña</label>
-                            <input
-                                className="input"
-                                type="password" 
-                                name="password" 
-                            />
-                        </div>
-                        <div className="forgot">Olvidaste tú contraseña?</div>
-                        <button className="submit">
-                            LOGIN
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+            </>
+        );
     }
-};
+}
 
 export default Login;
